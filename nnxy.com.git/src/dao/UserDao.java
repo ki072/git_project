165863a2b111317369f65d0f84d1ab2d218eeca8
ki072 <1916230511@qq.com>
2020-06-10 11:03:15 +0800

    用户模块 持久层操作DDL

---
+++ nnxy.com.git/src/dao/UserDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.util.List;
+
+public class UserDao {
+	public void addUser(int id,String name, String password) {
+		
+	}
+	public void deletUser(int id) {
+		
+	} 
+	public void updateUser(int id ,String newname, String newpassword) {
+		
+	} 
+	public List<Object> scanUser(int pageIndex,int pageSize) {
+		return null;
+	} 
+}
+
